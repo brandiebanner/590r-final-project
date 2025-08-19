1441786cf6ef38c4ce6bfae9fab8aef95c342e07
Brandie Shackelford <brandiebanner@gmail.com>
2025-08-19 18:25:43 -0400

    create regions

--- R/read in refugee data 590R.R
+++ R/read in refugee data 590R.R
@@ -3,3 +3,39 @@ refugees <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/t
 
 #https://github.com/rfordatascience/tidytuesday/blob/main/data/2023/2023-08-22/readme.md
 readr::write_rds(refugees, here::here("data", "refugees.rds"))
+
+levels(factor(refugees$coo_name))
+
+#code regions
+library(countrycode)
+
+manuallycode <- c(
+	"Stateless" = NA,
+	"Unknown" = NA,
+	"Tibetan" = "East Asia & Pacific",
+	"Western Sahara" = "Middle East & North Africa",
+	"Serbia and Kosovo: S/RES/1244 (1999)" = "Europe & Central Asia"
+)
+
+refugees$coo_name_fixed <- ifelse(
+	refugees$coo_name %in% names(manuallycode),
+	manuallycode[refugees$coo_name],
+	refugees$coo_name
+)
+
+manual_regions <- manuallycode[!is.na(manuallycode)]  # vector of manual region names
+
+refugees$coo_region <- NA_character_
+
+refugees$coo_region[refugees$coo_name %in% names(manuallycode)] <- refugees$coo_name_fixed[refugees$coo_name %in% names(manuallycode)]
+
+non_manual_idx <- !(refugees$coo_name %in% names(manuallycode))
+refugees$coo_region[non_manual_idx] <- countrycode(
+	refugees$coo_name_fixed[non_manual_idx],
+	origin = "country.name",
+	destination = "region"
+)
+
+refugees$coo_region[is.na(refugees$coo_region)] <- "Unknown"
+
+table(refugees$coo_region)
